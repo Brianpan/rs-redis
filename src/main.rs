99bacf81d238ef02feb7630be1f3f2dddfc629bf
Brianpan <b97705021@ntu.edu.tw>
2024-03-24 21:37:46 -0700

    configurable port

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
 mod engine;
 mod store;
 
+use clap::{Arg, Command};
 use engine::connection::handle_connection;
 use std::sync::Arc;
 use store::engine::StoreEngine;
 use tokio::{net::TcpListener, spawn};
 
+const PROGRAM_NAME: &str = "rs-redis";
+const VERSION: &str = "0.1.0";
+const DEFAULT_PORT: &str = "6379";
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    let db = Arc::new(StoreEngine::new());
+    // configure the command line arguments
+    let args = Command::new(PROGRAM_NAME)
+        .version(VERSION)
+        .about("redis in rust")
+        .arg(
+            Arg::new("port")
+                .help("Redis port")
+                .short('p')
+                .long("port")
+                .value_name("PORT")
+                .required(false),
+        )
+        .get_matches();
 
+    let binding = DEFAULT_PORT.to_string();
+    let redis_port = args.get_one::<String>("port").unwrap_or(&binding);
+    let redis_host = format!("0.0.0.0:{}", redis_port);
+
+    let listener = TcpListener::bind(redis_host).await.unwrap();
+    let db = Arc::new(StoreEngine::new());
     // reaper thread
     let reaper_db = db.clone();
     spawn(async move {
