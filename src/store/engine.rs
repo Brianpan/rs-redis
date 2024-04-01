e7cd37d692d4a1b6b9d3d37c858e1e5b9b00f4dd
Brianpan <b97705021@ntu.edu.tw>
2024-03-31 22:45:08 -0700

    hotfix

--- src/store/engine.rs
+++ src/store/engine.rs
@@ -247,14 +247,14 @@ impl StoreEngine {
             }
 
             // read rdb file
-            match stream.read(&mut buf) {
-                Ok(buf_len) => {
-                    let _ = String::from_utf8_lossy(buf[..buf_len].as_ref());
-                }
-                Err(e) => {
-                    return Err(anyhow::Error::new(e));
-                }
-            }
+            // match stream.read(&mut buf) {
+            //     Ok(buf_len) => {
+            //         let _ = String::from_utf8_lossy(buf[..buf_len].as_ref());
+            //     }
+            //     Err(e) => {
+            //         return Err(anyhow::Error::new(e));
+            //     }
+            // }
         }
 
         Ok(())
